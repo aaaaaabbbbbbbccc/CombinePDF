63f0d831b6f42099da3d690a45a5e9737af24123
shiva <vikbobinski@gmail.com>
2022-03-08 12:58:47 +0100

    itext merge pdf

---
+++ src/main/java/iTextMergePDF.java
@@ -0,0 +1,34 @@
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.PdfCopy;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.PdfSmartCopy;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class iTextMergePDF {
+    private static final String PATH_TO_FOLDER = "C:\\XX\\PDF";
+    private static final String MERGED_FILE_NAME = "mergedFile.pdf";
+
+    static public void main(String[] args) throws Exception{
+        mergePDF(PATH_TO_FOLDER, MERGED_FILE_NAME);
+    }
+    public static void mergePDF(String directory, String targetFile) throws DocumentException, IOException {
+        File dir = new File(directory);
+        File[] filesToMerge = dir.listFiles((file, fileName) -> fileName.endsWith(".pdf"));
+        Document document = new Document();
+        FileOutputStream outputStream = new FileOutputStream(PATH_TO_FOLDER + "\\" + targetFile);
+        PdfCopy copy = new PdfSmartCopy(document, outputStream);
+        document.open();
+
+        for (File inFile : filesToMerge) {
+            System.out.println(inFile.getCanonicalPath());
+            PdfReader reader = new PdfReader(inFile.getCanonicalPath());
+            copy.addDocument(reader);
+            reader.close();
+        }
+        document.close();
+    }
+}
